9e988cefc11196963c9c4245177527ddf637e401
shina1 <oswasiu@gmail.com>
2021-10-12 05:07:04 -0700

    bg/ fixed bug

--- highscore.js
+++ highscore.js
@@ -2,4 +2,4 @@ const highScoreList = document.getElementById("highScoreList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 // i want to add the high scores to the unorderd list in  the html page. that is mapping
 highScoreList.innerHTML = highScores.map(score => {
-    return `<li class="high-score">${score.name} :-- ${score.score}</li>`;}) .join("");
+    return `<li class="high-score">${score.name} : ${score.score}</li>`;}) .join("");
